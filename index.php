c5ee8cf3db74ed6b33c91c7f3210d4caecbc8a54
lemikeone <lemike10@gmail.com>
2017-03-05 14:22:00 +0100

    Affiche de la date du prochain anniversaire

--- index.php
+++ index.php
@@ -2,12 +2,23 @@
 $bdd = new PDO('mysql:host=localhost;dbname=gift-project;charset=utf8', 'root', 'root');
 $reponse = $bdd->query('SELECT * FROM usersfriends');
 
+ function get_next_birthday($birthday) {
+    $date = new DateTime($birthday);
+    $date->modify('+' . date('Y') - $date->format('Y') . ' years');
+    if($date < new DateTime()) {
+        $date->modify('+1 year');
+    }
+
+    return $date->format('Y-m-d');
+}
+
 // On affiche chaque entrée une à une
 while ($donnees = $reponse->fetch())
 {
+
 ?>
     <p>
-    <?php echo $donnees['prenom']; ?> <?php echo $donnees['Nom']; ?> est née le <?php echo $donnees['datedenaissance']; ?> 
+    <?php echo $donnees['prenom']; ?> <?php echo $donnees['Nom']; ?> : Son anniversaire est le <?php echo get_next_birthday($donnees['datedenaissance']); ?>
    </p><?php
    }
 
