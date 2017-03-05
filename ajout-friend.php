1329ed53b98a686610a09a4789d2f604e64d8c6d
lemikeone <lemike10@gmail.com>
2017-03-05 17:02:33 +0100

    Ajout du user ID aux proches

--- ajout-friend.php
+++ ajout-friend.php
@@ -1,15 +1,18 @@
 <?php  
+session_start();
+
 $bdd = new PDO('mysql:host=localhost;dbname=gift-project;charset=utf8', 'root', 'root');
 
 $nom = $_GET['nom'];
 $prenom = $_GET['prenom'];
 $birthdate = $_GET['birthdate'];
 
-$req = $bdd->prepare('INSERT INTO usersfriends(nom, prenom, datedenaissance) VALUES(:nom, :prenom, :datedenaissance)');
+$req = $bdd->prepare('INSERT INTO usersfriends(nom, prenom, datedenaissance, iduser) VALUES(:nom, :prenom, :datedenaissance, :iduser)');
 $req->execute(array(
 	'nom' => $nom,
 	'prenom' => $prenom,
 	'datedenaissance' => $birthdate,
+	'iduser' => $_SESSION['id'],
 	));
 
 echo 'Proche a été ajouté';
