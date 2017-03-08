20c5a86b927d5c8cea9625873a2091d1db6d806d
lemikeone <lemike10@gmail.com>
2017-03-08 21:45:50 +0100

    Correction de Noël pour qu'il s'affiche bien quelle que soit la date

--- index.php
+++ index.php
@@ -72,9 +72,9 @@ $reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR
          
         }
 
+// On affiche Noël si on est avant le 24 décembre de l'année en cours 
+if (date('m-d', time()) < "12-24") {
 ?>
-
-<!-- On affiche Noël -->
 <div class="flux">
 <h2>Noël</h2>
 <p><i class="fa fa-tree" aria-hidden="true"></i> Le 24 décembre</p>
@@ -82,8 +82,10 @@ $reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR
 
 <?php
 
-// On sort les anniversaires après Noël OU avant la date en cours   
-$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) > "12-24" OR SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6)  ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
+}
+
+// On sort les anniversaires après Noël  
+$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) > "12-24" ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
       $reponse->execute(array($_SESSION['id']));
 
       // On affiche chaque entrée une à une
@@ -109,6 +111,47 @@ $reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR
         <?php
          
         }
+
+// On affiche Noël si on est après le 24 décembre de l'année en cours 
+if (date('m-d', time()) > "12-24") {
+?>
+<div class="flux">
+<h2>Noël</h2>
+<p><i class="fa fa-tree" aria-hidden="true"></i> Le 24 décembre</p>
+</div>
+
+<?php }
+
+// On sort les anniversaires avant la date en cours   
+$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6)  ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
+      $reponse->execute(array($_SESSION['id']));
+
+      // On affiche chaque entrée une à une
+      while ($donnees = $reponse->fetch())
+        {
+
+        $nextbirthday = get_next_birthday($donnees['datedenaissance']);
+        $duree = floor((strtotime($nextbirthday) - time()));
+       ?>
+        <div class="flux">
+        
+
+        <h2><a href="fiche-proche.php?idproche=<?php echo $donnees['ID'] ?>"><?php echo $donnees['prenom']; ?> <?php echo $donnees['nom']; ?></a></h2>
+        <p><i class="fa fa-birthday-cake" aria-hidden="true"></i> Anniversaire : <?php echo (age($donnees['datedenaissance'])+1); ?> ans le <?php echo strftime("%A %e %B %Y", strtotime($nextbirthday)); ?></p>
+        <?php echo '<p ><i class="fa fa-clock-o" aria-hidden="true"></i> Dans ', floor((strtotime($nextbirthday) - time())/86400); echo " jours</p>"; 
+
+
+
+        ?> 
+
+        </div>
+
+        <?php
+         
+        }
+
+
+
 
 
         $reponse->closeCursor(); // Termine le traitement de la requête
