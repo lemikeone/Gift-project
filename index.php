027c0ef0983f8241e3859d4c15c9b3436ebe93be
lemikeone <lemike10@gmail.com>
2017-03-08 23:09:39 +0100

    Correction d'un bug sur Noël

--- index.php
+++ index.php
@@ -112,6 +112,34 @@ $reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR
          
         }
 
+// On sort les anniversaires avant la date en cours et avant Noël
+$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6) AND SUBSTR(`datedenaissance`,6) < "12-24" ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
+      $reponse->execute(array($_SESSION['id']));
+
+      // On affiche chaque entrée une à une
+      while ($donnees = $reponse->fetch())
+        {
+
+        $nextbirthday = get_next_birthday($donnees['datedenaissance']);
+        $duree = floor((strtotime($nextbirthday) - time()));
+       ?>
+        <div class="flux">
+        
+
+        <h2><a href="fiche-proche.php?idproche=<?php echo $donnees['ID'] ?>"><?php echo $donnees['prenom']; ?> <?php echo $donnees['nom']; ?></a></h2>
+        <p><i class="fa fa-birthday-cake" aria-hidden="true"></i> Anniversaire : <?php echo (age($donnees['datedenaissance'])+1); ?> ans le <?php echo strftime("%A %e %B %Y", strtotime($nextbirthday)); ?></p>
+        <?php echo '<p ><i class="fa fa-clock-o" aria-hidden="true"></i> Dans ', floor((strtotime($nextbirthday) - time())/86400); echo " jours</p>"; 
+
+
+
+        ?> 
+
+        </div>
+
+        <?php
+         
+        }
+
 // On affiche Noël si on est après le 24 décembre de l'année en cours 
 if (date('m-d', time()) > "12-24") {
 ?>
@@ -120,10 +148,12 @@ if (date('m-d', time()) > "12-24") {
 <p><i class="fa fa-tree" aria-hidden="true"></i> Le 24 décembre</p>
 </div>
 
-<?php }
+<?php
 
-// On sort les anniversaires avant la date en cours   
-$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6)  ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
+}
+
+// On sort les anniversaires avant la date en cours et apres Noël
+$reponse = $bdd->prepare('SELECT * FROM usersfriends WHERE iduser = ? AND SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6) AND SUBSTR(`datedenaissance`,6) > "12-24" ORDER BY CONCAT(SUBSTR(`datedenaissance`,6) < SUBSTR(CURDATE(),6), SUBSTR(`datedenaissance`,6))');
       $reponse->execute(array($_SESSION['id']));
 
       // On affiche chaque entrée une à une
